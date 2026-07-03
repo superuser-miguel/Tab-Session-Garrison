826752780937d8a7d0200be376503a894910ab5b
superuser-miguel <16271056+superuser-miguel@users.noreply.github.com>
2026-07-03 13:58:16 -0400

    auto: 2026-07-03 13:58

--- src/popup/components/Header.js
+++ src/popup/components/Header.js
@@ -61,7 +61,7 @@ export default props => {
 
   return (
     <div id="header">
-      <div className="title">Tab Session Manager</div>
+      <div className="title">Tab Session Garrison</div>
       <div className="rightButtons">
         {shouldShowCloudSync && <SyncStatus syncStatus={syncStatus} />}
         <button
