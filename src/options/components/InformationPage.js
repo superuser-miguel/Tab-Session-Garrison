7749761048ade4518a5812dce32a1d03d3e693fc
superuser-miguel <16271056+superuser-miguel@users.noreply.github.com>
2026-07-03 14:07:05 -0400

    auto: 2026-07-03 14:07

--- src/options/components/InformationPage.js
+++ src/options/components/InformationPage.js
@@ -1,31 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import browser from "webextension-polyfill";
-import browserInfo from "browser-info";
 import queryString from "query-string";
 import OptionsContainer from "./OptionContainer";
 import manifest from "src/manifest.json";
 
+const repoUrl = "https://github.com/superuser-miguel/Tab-Session-Garrison";
+
 export default props => {
   const query = queryString.parse(props.location.search);
-
   const extensionVersion = manifest.version;
-  const isChrome = browserInfo().name == "Chrome";
-  const paypalLink = `https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&no_shipping=1&business=sienori.firefox@gmail.com&item_name=Tab Session Manager ${
-    isChrome ? "for Chrome " : ""
-  }- Donation`;
-  const patreonLink = "https://www.patreon.com/sienori";
-  const email = `sienori.firefox+tsm${isChrome ? "fc" : ""}@gmail.com`;
-
-  const [sponsorsHeihgt, setSponsorsHeight] = useState();
-
-  useEffect(() => {
-    const setHeight = e => {
-      if (e.data[0] !== "setSponsorsHeight") return;
-      setSponsorsHeight(e.data[1]);
-    };
-    window.addEventListener("message", setHeight);
-    return () => window.removeEventListener("message", setHeight);
-  });
 
   return (
     <div>
@@ -38,15 +21,8 @@ export default props => {
         updated={query.action === "updated"}
         extraCaption={
           <p className="caption">
-            <a href="https://github.com/sienori/Tab-Session-Manager/releases" target="_blank">
+            <a href={`${repoUrl}/releases`} target="_blank">
               Version {extensionVersion}
-            </a>
-            <span>　</span>
-            <a
-              href="https://github.com/sienori/Tab-Session-Manager/blob/master/BACKERS.md"
-              target="_blank"
-            >
-              {browser.i18n.getMessage("backersLabel")}
             </a>
           </p>
         }
@@ -54,57 +30,9 @@ export default props => {
 
       <OptionsContainer
         title={"licenseLabel"}
-        captions={["Mozilla Public License, Version. 2.0"]}
+        captions={["GNU General Public License, Version 3.0 or later"]}
         useRawCaptions={true}
         type={"none"}
-      />
-      <hr />
-      <OptionsContainer title={"donationLabel"} captions={["donationCaptionLabel"]} type={"none"} />
-      <OptionsContainer
-        title={""}
-        captions={[""]}
-        type={"none"}
-        extraCaption={
-          <div>
-            <a href={patreonLink} target="_blank">
-              <img
-                src="/icons/patreonButton.png"
-                alt="Patreon"
-                style={{ height: 44, marginRight: 20 }}
-              />
-            </a>
-            <a href={paypalLink} target="_blank">
-              <img src="/icons/paypalButton.png" alt="Paypal" />
-            </a>
-          </div>
-        }
-      />
-      <OptionsContainer
-        title={""}
-        captions={[""]}
-        type={"none"}
-        extraCaption={
-          <div>
-            <p className="caption">
-              <a className="amazonUrl" href={browser.i18n.getMessage("amazonUrl")} target="_blank">
-                {browser.i18n.getMessage("amazonTitleLabel")}
-              </a>
-            </p>
-            <p className="caption">email: {email}</p>
-          </div>
-        }
-      />
-      <hr />
-      <OptionsContainer
-        title={"sponsorsLabel"}
-        captions={[""]}
-        type={"none"}
-        extraCaption={
-          <iframe
-            src="https://tab-session-manager.sienori.com/sponsors.html"
-            style={{ height: sponsorsHeihgt, marginTop: 10 }}
-          />
-        }
       />
       <hr />
       <OptionsContainer
@@ -114,33 +42,8 @@ export default props => {
         extraCaption={
           <div>
             <p>
-              <a
-                href="https://addons.mozilla.org/firefox/addon/tab-session-manager/?src=optionpage"
-                target="_blank"
-              >
-                {browser.i18n.getMessage("firefoxLabel")}
-              </a>
-              <span>　</span>
-              <a
-                href="https://chrome.google.com/webstore/detail/tab-session-manager/iaiomicjabeggjcfkbimgmglanimpnae"
-                target="_blank"
-              >
-                {browser.i18n.getMessage("chromeLabel")}
-              </a>
-              <span>　</span>
-              <a
-                href="https://microsoftedge.microsoft.com/addons/detail/tab-session-manager/jkjjclfiflhpjangefhgfjhgfbhajadk"
-                target="_blank"
-              >
-                {browser.i18n.getMessage("edgeLabel")}
-              </a>
-              <span>　</span>
-              <a href="https://github.com/sienori/Tab-Session-Manager" target="_blank">
+              <a href={repoUrl} target="_blank">
                 GitHub
-              </a>
-              <span>　</span>
-              <a href="https://tab-session-manager.sienori.com/privacy-policy" target="_blank">
-                {browser.i18n.getMessage("privacyPolicyLabel")}
               </a>
             </p>
           </div>
