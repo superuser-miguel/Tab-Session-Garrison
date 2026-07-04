c6fbe8b2b17b9dd2a9c9e110f6133351a78488b5
superuser-miguel <16271056+superuser-miguel@users.noreply.github.com>
2026-07-04 15:52:11 -0400

    auto: 2026-07-04 15:52

--- src/popup/actions/generateTagLabel.js
+++ src/popup/actions/generateTagLabel.js
@@ -2,6 +2,8 @@ import React from "react";
 import browser from "webextension-polyfill";
 import TagIcon from "../icons/tag.svg";
 import AutoSaveIcon from "../icons/update.svg";
+import WindowCloseIcon from "../icons/windowClose.svg";
+import BrowserExitIcon from "../icons/browserExit.svg";
 import StartupIcon from "../icons/star.svg";
 import TrackingIcon from "../icons/circle.svg";
 
@@ -25,14 +27,31 @@ export const generateTagLabel = tag => {
 export const generateTagIcon = tag => {
   switch (tag) {
     case "regular":
-    case "winClose":
-    case "browserExit":
       return <AutoSaveIcon className="autoSaveIcon" />;
+    case "winClose":
+      return <WindowCloseIcon className="winCloseIcon" />;
+    case "browserExit":
+      return <BrowserExitIcon className="browserExitIcon" />;
     case "_startup":
       return <StartupIcon className="startupIcon" />;
     case "_tracking":
       return <TrackingIcon className="trackingIcon" />;
     default:
       return <TagIcon className="tagIcon" />;
+  }
+};
+
+// Per-type modifier class so each auto-save kind gets its own colour in the
+// pill (see the *.scss tagRegular/tagWinClose/tagBrowserExit rules).
+export const generateTagClass = tag => {
+  switch (tag) {
+    case "regular":
+      return "tagRegular";
+    case "winClose":
+      return "tagWinClose";
+    case "browserExit":
+      return "tagBrowserExit";
+    default:
+      return "";
   }
 };
