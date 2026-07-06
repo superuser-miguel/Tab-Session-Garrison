108a10f354cf2f1ad9f820e3a9950211a2fdbe90
superuser-miguel <16271056+superuser-miguel@users.noreply.github.com>
2026-07-06 10:54:52 -0400

    auto: 2026-07-06 10:54

--- src/popup/components/SessionItem.js
+++ src/popup/components/SessionItem.js
@@ -3,7 +3,12 @@ import browser from "webextension-polyfill";
 import Highlighter from "react-highlight-words";
 import { getSettings } from "src/settings/settings";
 import { sendOpenMessage } from "../actions/controlSessions";
-import { generateTagLabel, generateTagIcon, generateTagClass } from "../actions/generateTagLabel";
+import {
+  generateTagLabel,
+  generateTagIcon,
+  generateTagClass,
+  getDisplayTags
+} from "../actions/generateTagLabel";
 import { tabGroupColorHex } from "../../common/tabGroupUtils";
 import generateWindowsInfo from "../actions/generateWindowsInfo";
 import SessionMenuItems from "./SessionMenuItems";
